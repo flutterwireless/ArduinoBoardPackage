ca22428ab4916177aac620c12b95da6517d175c2
mjs513 <CyberMerln@aol.com>
2017-10-01 05:42:54 -0400

    Updated with 0.1.4 Changes as necessary

--- flutter/hardware/sam/0.1.3/libraries/Wire/src/Wire.h
+++ flutter/hardware/sam/0.1.3/libraries/Wire/src/Wire.h
@@ -28,6 +28,7 @@
 #include "variant.h"
 
 #define BUFFER_LENGTH 32
+
 #define TWBR int _TWBR
  // WIRE_HAS_END means Wire has end()
 #define WIRE_HAS_END 1
@@ -43,12 +44,13 @@ public:
 	void beginTransmission(uint8_t);
 	void beginTransmission(int);
 	uint8_t endTransmission(void);
-    uint8_t endTransmission(uint8_t);
+  uint8_t endTransmission(uint8_t);
 	uint8_t requestFrom(uint8_t, uint8_t);
-    uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
+	uint8_t requestFrom(byte, uint16_t);
+  uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
 	uint8_t requestFrom(uint8_t, uint8_t, uint32_t, uint8_t, uint8_t);
 	uint8_t requestFrom(int, int);
-    uint8_t requestFrom(int, int, int);
+  uint8_t requestFrom(int, int, int);
 	virtual size_t write(uint8_t);
 	virtual size_t write(const uint8_t *, size_t);
 	virtual int available(void);
